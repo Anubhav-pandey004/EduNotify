9707902d5a80e40482445d705f9cc0742a5fbc7b
Anubhav <110095813+Anubhav-pandey004@users.noreply.github.com>
2025-06-01 19:04:32 +0530

    Update login.js

--- controller/login.js
+++ controller/login.js
@@ -39,6 +39,7 @@ const login=async(req,res)=>{
             const token = await jwt.sign(tokendata, process.env.JWT_SECRET, {expiresIn : "8h"})
 
             const tokenOption = {
+               sameSite: "None", 
                 httpOnly : true,
                 secure:true
             }
